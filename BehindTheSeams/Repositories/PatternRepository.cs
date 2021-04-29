0c2997c3c949bf2a4f60b7dc2a768cb58f197288
Aaron Resch <aaronmresch@gmail.com>
2021-04-28 19:10:14 -0500

    added pattern sizes to pattern objects

--- BehindTheSeams/Repositories/PatternRepository.cs
+++ BehindTheSeams/Repositories/PatternRepository.cs
@@ -24,11 +24,15 @@ namespace BehindTheSeams.Repositories
                         SELECT p.Id, p.UserId, p.[Url], p.[Name], p.PublisherId, p.PurchaseDate, p.FabricTypeId, p.Notes, p.CategoryId,
 	                        pub.[Name] AS PublisherName, pub.[Url] AS PublisherUrl,
 	                        f.[Name] AS FabricTypeName,
-	                        c.[Name] AS CategoryName
+	                        c.[Name] AS CategoryName,
+                            s.[Name] AS SizeName, s.[Id] AS SizeId, s.Abbreviation,
+                            ps.Yards, ps.Id AS PatternSizeId
                         FROM Pattern p
 	                        LEFT JOIN Publisher pub ON p.PublisherId = pub.Id
 	                        LEFT JOIN FabricType f ON p.FabricTypeId = f.Id
 	                        LEFT JOIN Category c ON p.CategoryId = c.Id
+                            LEFT JOIN PatternSize ps ON p.Id = ps.PatternId
+                            LEFT JOIN Size s ON s.Id = ps.SizeId
                         WHERE p.UserId = @UserId";
                     DbUtils.AddParameter(cmd, "@UserId", userId);
 
@@ -36,7 +40,31 @@ namespace BehindTheSeams.Repositories
                     var patterns = new List<Pattern>();
                     while (reader.Read())
                     {
-                        patterns.Add(NewPatternFromDb(reader));
+                        var patternId = DbUtils.GetInt(reader, "Id");
+                        var existingPattern = patterns.FirstOrDefault(p => p.Id == patternId);
+                        if (existingPattern == null)
+                        {
+                            existingPattern = NewPatternFromDb(reader);
+                            existingPattern.PatternSizes = new List<PatternSize>();
+                            patterns.Add(existingPattern);
+                        }
+
+                        if (DbUtils.IsNotDbNull(reader, "SizeId"))
+                        {
+                            existingPattern.PatternSizes.Add(new PatternSize()
+                            {
+                                Id = DbUtils.GetInt(reader, "PatternSizeId"),
+                                SizeId = DbUtils.GetInt(reader, "SizeId"),
+                                PatternId = DbUtils.GetInt(reader, "Id"),
+                                Yards = DbUtils.GetDecimal(reader, "Yards"),
+                                Size = new Size()
+                                {
+                                    Id = DbUtils.GetInt(reader, "SizeId"),
+                                    Name = DbUtils.GetString(reader, "SizeName"),
+                                    Abbreviation = DbUtils.GetString(reader, "Abbreviation")
+                                }
+                            });
+                        }
                     }
                     reader.Close();
                     return patterns;
@@ -55,22 +83,47 @@ namespace BehindTheSeams.Repositories
                         SELECT p.Id, p.UserId, p.[Url], p.[Name], p.PublisherId, p.PurchaseDate, p.FabricTypeId, p.Notes, p.CategoryId,
 	                        pub.[Name] AS PublisherName, pub.[Url] AS PublisherUrl,
 	                        f.[Name] AS FabricTypeName,
-	                        c.[Name] AS CategoryName
+	                        c.[Name] AS CategoryName,
+                            s.[Name] AS SizeName, s.[Id] AS SizeId, s.Abbreviation,
+                            ps.Yards, ps.Id AS PatternSizeId
                         FROM Pattern p
 	                        LEFT JOIN Publisher pub ON p.PublisherId = pub.Id
 	                        LEFT JOIN FabricType f ON p.FabricTypeId = f.Id
 	                        LEFT JOIN Category c ON p.CategoryId = c.Id
+                            LEFT JOIN PatternSize ps ON p.Id = ps.PatternId
+                            LEFT JOIN Size s ON s.Id = ps.SizeId
                         WHERE p.Id = @Id";
                     DbUtils.AddParameter(cmd, "@Id", id);
 
-                    Pattern pattern = null;
-
                     var reader = cmd.ExecuteReader();
 
-                    if (reader.Read())
+                    Pattern pattern = null;
+
+                    while (reader.Read())
                     {
-                        pattern = NewPatternFromDb(reader);
-                    }
+                        if (pattern == null)
+                        {
+                            pattern = NewPatternFromDb(reader);
+                            pattern.PatternSizes = new List<PatternSize>();
+                        }
+                        if (DbUtils.IsNotDbNull(reader, "SizeId"))
+                        {
+                            pattern.PatternSizes.Add(new PatternSize()
+                            {
+                                Id = DbUtils.GetInt(reader, "PatternSizeId"),
+                                SizeId = DbUtils.GetInt(reader, "SizeId"),
+                                PatternId = DbUtils.GetInt(reader, "Id"),
+                                Yards = DbUtils.GetDecimal(reader, "Yards"),
+                                Size = new Size()
+                                {
+                                    Id = DbUtils.GetInt(reader, "SizeId"),
+                                    Name = DbUtils.GetString(reader, "SizeName"),
+                                    Abbreviation = DbUtils.GetString(reader, "Abbreviation")
+                                }
+                            });
+                        }
+                    }                    
+                    
                     reader.Close();
 
                     return pattern;
